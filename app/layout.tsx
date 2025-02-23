4620246b75c3a011a7407a6e4919e78cacf613c3
Heyru Jemal <heyru638@gmail.com>
2025-02-23 12:21:20 +0300

    description

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Heyru's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Modern & Minimal DEveloper Heyru's Portfolio",
 };
 
 export default function RootLayout({
