66634229a5d353ea16b537ab82f0e23c9443514c
Heyru Jemal <heyru638@gmail.com>
2025-08-11 22:02:52 +0300

    project Modified

--- data/index.ts
+++ data/index.ts
@@ -87,11 +87,17 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/Devheyru",
+    title: "Minno Delivery App",
+    des: "A Food Delivery Mobile App Currently I am developing it with Flutter and Firebase. it will be released soon",
+    img: "/logo.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/Laravel.svg",
+      "/flutter.svg",
+      "/firebase.svg",
+    ],
+    link: "https://github.com/Devheyru/Delivery",
   },
   {
     id: 3,
